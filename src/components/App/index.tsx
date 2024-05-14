0e5f98f66700b4d5c50f15e50aba88c669f54197
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-05-14 06:22:15 +0200

    first commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import CreateEmployee from '../../pages/CreateEmployee'
+import EmployeeList from '../../pages/EmployeeList'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<CreateEmployee />} />
+      <Route path="/employeelist" element={<EmployeeList />} />
+    </Routes>
+  )
+}
---
+++ src/pages/CreateEmployee.tsx
@@ -0,0 +1,7 @@
+export default function CreateEmployee() {
+  return (
+    <div className="flex items-center justify-center m-auto sm:mt-[100px] mt-[20px]">
+      CreateEmployee
+    </div>
+  )
+}
