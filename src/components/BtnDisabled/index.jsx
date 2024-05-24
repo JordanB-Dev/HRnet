381ecf7839c301be0880c95dd53b5b3ea9cfcc16
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-05-24 07:31:19 +0200

    Btn

--- src/components/BtnDisabled/index.jsx
+++ src/components/BtnDisabled/index.jsx
@@ -2,7 +2,7 @@ const BtnDisabled = ({ title }) => {
   return (
     <button
       title="Button Disabled"
-      className="dark:bg-primary2 bg-primary select-none align-middlefont-bold ring-offset-background focus-visible:ring-ring focus-visible:ring-offset-2  align-middle font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-md text-primaryforeground focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none w-3/6 sm:w-2/5"
+      className="bg-primary select-none align-middlefont-bold ring-offset-background focus-visible:ring-ring focus-visible:ring-offset-2 align-middle font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-md text-primaryforeground focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none w-3/6 sm:w-2/5"
       disabled
     >
       {title}
